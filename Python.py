366f8803ef24e3f6d8a8c57ed8e5aed6905ee8f7
PabloMSan <pablom.sanala@gmail.com>
2021-02-05 11:39:53 +0100

    New Python Script

---
+++ Python.py
@@ -0,0 +1,17 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+
+x = np.linspace(0,7,100)
+y = np.sin(x)
+
+
+
+fig, ax = plt.subplots(1,1)
+
+ax.plot(x,y,"r-")
+ax.set_xlabel("Angulo")
+ax.set_ylabel("Seno")
+
+
